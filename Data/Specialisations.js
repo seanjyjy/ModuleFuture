8f8b6c6eaca517341a43b36b71def61d5964d3d8
keanecjy <e0425601@u.nus.edu>
2020-06-24 18:40:23 +0800

    Foucs Areas half done

---
+++ Data/Specialisations.js
@@ -0,0 +1,14 @@
+const specialisations = [
+  { key: 1, name: "Algorithms & Theory" },
+  { key: 2, name: "Artificial Intelligence" },
+  { key: 3, name: "Computer Graphics and Games" },
+  { key: 4, name: "Computer Security" },
+  { key: 5, name: "Database Systems" },
+  { key: 6, name: "Multimedia Information Retrieval" },
+  { key: 7, name: "Networking and Distributed Systems" },
+  { key: 8, name: "Parallel Computing" },
+  { key: 9, name: "Programming Languages" },
+  { key: 10, name: "Software Engineering" },
+];
+
+export default specialisations;
