b70fd3d713611735798d400d34b9dfb2e07c1782
seanjyjy <e0406739@u.nus.edu>
2020-06-24 18:48:44 +0800

    updated globalFont

--- Component/GlobalFont.js
+++ Component/GlobalFont.js
@@ -7,6 +7,10 @@ export const globalFontStyles = StyleSheet.create({
     fontSize: 15,
   },
   // ------------------------------------------OPENSANS-REGULAR------------------------------------------------------
+  OSR_12: {
+    fontFamily: "OpenSans-Regular",
+    fontSize: 12,
+  },
   OSR_13: {
     fontFamily: "OpenSans-Regular",
     fontSize: 13,
@@ -97,6 +101,14 @@ export const globalFontStyles = StyleSheet.create({
   },
 
   // ---------------------------------------NUNITO-BOLD-------------------------------------------------------------
+  NB_14: {
+    fontFamily: "Nunito-Bold",
+    fontSize: 14,
+  },
+  NB_15: {
+    fontFamily: "Nunito-Bold",
+    fontSize: 15,
+  },
   NB_17: {
     fontFamily: "Nunito-Bold",
     fontSize: 17,
