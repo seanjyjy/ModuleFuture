f15632045944640909fdc46a17c5aa2059b7b5c0
seanjyjy <e0406739@u.nus.edu>
2020-07-19 05:19:55 +0800

    -UI design test credits page

--- Screens/HomepageScreens/Profile/Credit.js
+++ Screens/HomepageScreens/Profile/Credit.js
@@ -5,11 +5,17 @@ import {
   Text,
   TouchableOpacity,
   ScrollView,
+  ImageBackground,
+  Dimensions,
 } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import Ionicons from "react-native-vector-icons/Ionicons";
 import Header from "../../../Component/Header";
 import { globalFontStyles } from "../../../Component/GlobalFont";
+
+const width = Dimensions.get("window").width;
+const height = Dimensions.get("window").height;
+
 const Credit = () => {
   const navigation = useNavigation();
   return (
@@ -31,64 +37,85 @@ const Credit = () => {
         }
         rightChildren={<View />}
       />
+
       <View
         style={{
-          height: "82%",
-          width: "92%",
-          alignSelf: "center",
-          top: 15,
-          paddingHorizontal: 20,
-          paddingVertical: 10,
-          backgroundColor: "white",
-          borderRadius: 20,
-          shadowColor: "#000",
+          width: width - 26,
+          height: 0.82 * height,
+          top: 10,
           shadowOffset: {
-            width: 0,
+            width: 3,
             height: 2,
           },
           shadowOpacity: 0.25,
           shadowRadius: 3.84,
+          backgroundColor: "white",
           elevation: 5,
+          borderBottomLeftRadius: 20,
+          borderBottomRightRadius: 20,
+          borderTopRightRadius: 20,
+          borderTopLeftRadius: 20,
+          overflow: Platform.OS === "android" ? "hidden" : null,
+          alignSelf: "center",
         }}
       >
-        <ScrollView>
-          <Text style={styles.biggestWords}>About</Text>
-          <Text style={styles.biggerWords}>
-            This app was initially customized for SoC students. Since then, we
-            have extended its functionality to all other courses as well! If
-            there is something you wish to be included in the app, do suggest it
-            to us!
-          </Text>
-          <Text style={styles.biggestWords}>Developers</Text>
-          <Text style={styles.biggerWords}>Keane Chan Jun Yu</Text>
-          <Text style={styles.biggerWords}>Lum Jian Yang Sean</Text>
-          <Text style={styles.biggestWords}>Photo Credits</Text>
-          <Text style={styles.biggerWords}>
-            Photo by Tirachard Kumtanom from Pexels
-          </Text>
-          <Text style={styles.biggerWords}>Photo by PixaBay from Pexels</Text>
-          <Text style={styles.biggerWords}>
-            Photo by Jessica Lewis from Pexels
-          </Text>
-          <Text style={styles.biggerWords}>
-            Photo by Jess Bailey Designs from Pexels
-          </Text>
-          <Text style={styles.biggerWords}>Photo by Bich Tran from Pexels</Text>
-          <Text style={styles.biggerWords}>
-            Photo by Madison Inouye from Pexels
-          </Text>
-          <Text style={styles.biggerWords}>
-            Photo by Content Pixie from Pexels
-          </Text>
-        </ScrollView>
-        <View style={{ height: 25 }}>
-          <Text
-            style={styles.tncStyle}
-            onPress={() => navigation.navigate("TnC", { fromWhere: "Credit" })}
-          >
-            Terms & Conditions.
-          </Text>
-        </View>
+        <ImageBackground
+          source={require("../../../assets/aboutbg.png")}
+          style={{
+            flex: 1,
+            resizeMode: "cover",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+          imageStyle={{ borderRadius: 20 }}
+        >
+          <View style={styles.creditDesign}>
+            <ScrollView style={{ marginHorizontal: 20 }}>
+              <Text style={styles.biggestWords}>About</Text>
+              <Text style={styles.biggerWords}>
+                This app was initially customized for SoC students. Since then,
+                we have extended its functionality to all other courses as well!
+                If there is something you wish to be included in the app, do
+                suggest it to us!
+              </Text>
+              <Text style={styles.biggestWords}>Developers</Text>
+              <Text style={styles.biggerWords}>Keane Chan Jun Yu</Text>
+              <Text style={styles.biggerWords}>Lum Jian Yang Sean</Text>
+              <Text style={styles.biggestWords}>Photo Credits</Text>
+              <Text style={styles.biggerWords}>
+                Photo by Tirachard Kumtanom from Pexels
+              </Text>
+              <Text style={styles.biggerWords}>
+                Photo by PixaBay from Pexels
+              </Text>
+              <Text style={styles.biggerWords}>
+                Photo by Jessica Lewis from Pexels
+              </Text>
+              <Text style={styles.biggerWords}>
+                Photo by Jess Bailey Designs from Pexels
+              </Text>
+              <Text style={styles.biggerWords}>
+                Photo by Bich Tran from Pexels
+              </Text>
+              <Text style={styles.biggerWords}>
+                Photo by Madison Inouye from Pexels
+              </Text>
+              <Text style={styles.biggerWords}>
+                Photo by Content Pixie from Pexels
+              </Text>
+            </ScrollView>
+            <View style={{ height: 40 }}>
+              <Text
+                style={styles.tncStyle}
+                onPress={() =>
+                  navigation.navigate("TnC", { fromWhere: "Credit" })
+                }
+              >
+                Terms & Conditions.
+              </Text>
+            </View>
+          </View>
+        </ImageBackground>
       </View>
     </View>
   );
@@ -108,6 +135,7 @@ const styles = StyleSheet.create({
     marginTop: 10,
     ...globalFontStyles.NSB_13,
     left: 5,
+    color: "#232323",
   },
   biggestWords: {
     left: 5,
@@ -122,5 +150,13 @@ const styles = StyleSheet.create({
     ...globalFontStyles.NB_13,
     color: "#51739B",
     top: 1,
+  },
+  creditDesign: {
+    height: "100%",
+    width: "95%",
+    alignSelf: "center",
+    paddingVertical: 5,
+    backgroundColor: "transparent",
+    overflow: "hidden",
   },
 });
