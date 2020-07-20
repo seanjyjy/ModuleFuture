4d07a5aeef59f11f61f0c774dc64c9fbb7753f84
seanjyjy <e0406739@u.nus.edu>
2020-07-20 21:57:11 +0800

    mini tutorial done

--- Screens/HomepageScreens/Planner/ContentPage.js
+++ Screens/HomepageScreens/Planner/ContentPage.js
@@ -18,11 +18,17 @@ import CardWallet from "../../../Component/CardWallet";
 import FontisoIcon from "react-native-vector-icons/Fontisto";
 import { Menu } from "../../../Data/CardList";
 import FirebaseDB from "../../../FirebaseDB";
+import { useSafeArea, SafeAreaView } from "react-native-safe-area-context";
+import { copilot, walkthroughable, CopilotStep } from "react-native-copilot";
+import Button from "./Button";
 const width = Dimensions.get("window").width;
 const height = Dimensions.get("window").height;
 const AnimatedFlatList = Animated.createAnimatedComponent(FlatList);
-
+const WalkthroughableView = walkthroughable(View);
+const WalkthroughableTouchableOpacity = walkthroughable(TouchableOpacity);
+const WalkthroughableSafeAreaView = walkthroughable(SafeAreaView);
 const ContentPage = (props) => {
+  const usaB = useSafeArea().bottom;
   const navigation = useNavigation();
   const isFocused = useIsFocused();
   const userInfo = FirebaseDB.firestore().collection("users");
@@ -56,6 +62,9 @@ const ContentPage = (props) => {
   const [y, setY] = useState(new Animated.Value(0));
 
   useEffect(() => {
+    if (!FirebaseDB.auth().currentUser.emailVerified) {
+      props.start();
+    }
     const unsub = userInfo.doc(userID).onSnapshot((document) => {
       const data = document.data();
       const val = data.expectedSemGrad;
@@ -120,27 +129,36 @@ const ContentPage = (props) => {
               alignItems: "center",
             }}
           >
-            <FontisoIcon
-              name="favorite"
-              size={22}
-              color="#E59090"
-              style={{ right: 0.05 * width, bottom: 0.018 * height }}
-              onPress={() => {
-                if (favPlanArray.length > 0 && favPlanInfo.length > 0) {
-                  navigation.navigate("ViewPlan", {
-                    item: [title, docLoc, size, fromWhere, dataArray, true],
-                  });
-                } else {
-                  Alert.alert(
-                    "Oops",
-                    "You have not selected a favourite plan yet!",
-                    [{ text: "Cancel", onPress: () => {} }],
-                    { cancelable: false }
-                  );
-                }
-              }}
-            />
+            <CopilotStep
+              text="This is where you can select your favourite plans!"
+              order={2}
+              name="favourite plans"
+            >
+              <WalkthroughableView style={styles.walkableIcon}>
+                <FontisoIcon
+                  name="favorite"
+                  size={22}
+                  color="#E59090"
+                  style={{ bottom: 0.018 * height }}
+                  onPress={() => {
+                    if (favPlanArray.length > 0 && favPlanInfo.length > 0) {
+                      navigation.navigate("ViewPlan", {
+                        item: [title, docLoc, size, fromWhere, dataArray, true],
+                      });
+                    } else {
+                      Alert.alert(
+                        "Oops",
+                        "You have not selected a favourite plan yet!",
+                        [{ text: "Cancel", onPress: () => {} }],
+                        { cancelable: false }
+                      );
+                    }
+                  }}
+                />
+              </WalkthroughableView>
+            </CopilotStep>
           </View>
+
           <View
             style={{
               flex: 2,
@@ -148,16 +166,7 @@ const ContentPage = (props) => {
               alignItems: "center",
             }}
           >
-            <Text
-              style={{
-                ...globalFontStyles.NB_28,
-                color: "#FB5581",
-                bottom: 5,
-                left: 3,
-              }}
-            >
-              Planner
-            </Text>
+            <Text style={styles.plannerTextDesign}>Planner</Text>
           </View>
 
           <View
@@ -167,34 +176,28 @@ const ContentPage = (props) => {
               alignItems: "center",
             }}
           >
-            <TouchableOpacity
-              onPress={() => {
-                navigation.navigate("ProgressPage", {
-                  usersDetails: usersDetails,
-                  from: "ContentPage",
-                  userID: userID,
-                  gradSem: gradSem,
-                });
-              }}
-              style={{
-                width: 50,
-                height: 50,
-                justifyContent: "center",
-                alignItems: "center",
-                overflow: "hidden",
-              }}
+            <CopilotStep
+              text="This is where you can view your progress"
+              order={3}
+              name="progress page"
             >
-              {/* <Entypo
-                name="bar-graph"
-                color="#A5A0A0"
-                size={25}
-                style={{ left: 25, top: 0.015 * height }}
-              /> */}
-              <Image
-                source={require("../../../assets/bargraph.png")}
-                style={{ left: 10, resizeMode: "contain", flex: 1, bottom: 3 }}
-              />
-            </TouchableOpacity>
+              <WalkthroughableTouchableOpacity
+                onPress={() => {
+                  navigation.navigate("ProgressPage", {
+                    usersDetails: usersDetails,
+                    from: "ContentPage",
+                    userID: userID,
+                    gradSem: gradSem,
+                  });
+                }}
+                style={styles.barGraphHolderDesign}
+              >
+                <Image
+                  source={require("../../../assets/bargraph.png")}
+                  style={{ resizeMode: "contain", flex: 1, bottom: 3 }}
+                />
+              </WalkthroughableTouchableOpacity>
+            </CopilotStep>
           </View>
         </ImageBackground>
       </View>
@@ -206,34 +209,162 @@ const ContentPage = (props) => {
         }}
       />
 
-      <View style={{ flex: 1, backgroundColor: "#f9f9f9" }}>
-        <AnimatedFlatList
-          showsVerticalScrollIndicator={false}
-          scrollEventThrottle={16}
-          bounces={false}
-          data={cardArray}
-          keyExtractor={(item) => item.key.toString()}
-          renderItem={({ item }) => {
-            return CardWallet(y, item.key.toString(), item.card, () => [
-              userID,
-              item.PageName,
-              totalSumTotalMc(selectedplansinfo, item.PageName),
-              selectedplansinfo,
-              navigation,
-            ]);
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: "#f9f9f9",
+        }}
+      >
+        <CopilotStep
+          text="This is where you can select your plans!"
+          order={1}
+          name="plans"
+        >
+          <WalkthroughableView
+            style={{
+              height: 0.887 * height - 80 - usaB,
+              width: "100%",
+              top: 20,
+            }}
+          >
+            <View
+              style={{
+                height: 0.887 * height - 65 - usaB,
+                width: "100%",
+                bottom: 20,
+              }}
+            >
+              <AnimatedFlatList
+                showsVerticalScrollIndicator={false}
+                scrollEventThrottle={16}
+                bounces={false}
+                data={cardArray}
+                keyExtractor={(item) => item.key.toString()}
+                renderItem={({ item }) => {
+                  return CardWallet(y, item.key.toString(), item.card, () => [
+                    userID,
+                    item.PageName,
+                    totalSumTotalMc(selectedplansinfo, item.PageName),
+                    selectedplansinfo,
+                    navigation,
+                  ]);
+                }}
+                {...{ onScroll }}
+              />
+            </View>
+          </WalkthroughableView>
+        </CopilotStep>
+        <View
+          style={{
+            height: 65,
+            width: "100%",
+            flexDirection: "row",
           }}
-          {...{ onScroll }}
-        />
+        >
+          <CopilotStep
+            text="This is where you can select and create your plans!"
+            order={4}
+            name="planner"
+          >
+            <WalkthroughableView style={styles.areaPerIcon} />
+          </CopilotStep>
+          <CopilotStep
+            text="This is where you can view and edit your records"
+            order={5}
+            name="records"
+          >
+            <WalkthroughableView style={styles.areaPerIcon} />
+          </CopilotStep>
+          <CopilotStep
+            text="This is where you can view information on the focus area"
+            order={6}
+            name="focus"
+          >
+            <WalkthroughableView style={styles.areaPerIcon} />
+          </CopilotStep>
+          <CopilotStep
+            text="This is where you can search for information on the modules"
+            order={7}
+            name="module"
+          >
+            <WalkthroughableView style={styles.areaPerIcon} />
+          </CopilotStep>
+          <CopilotStep
+            text="This is where you can edit your profile as well as view some pro-tips! (Recommended to view)"
+            order={8}
+            name="profile"
+          >
+            <WalkthroughableView style={styles.areaPerIcon} />
+          </CopilotStep>
+        </View>
       </View>
     </View>
   );
 };
-
-export default ContentPage;
+const TooltipComponent = ({
+  isFirstStep,
+  isLastStep,
+  handleNext,
+  handlePrev,
+  handleStop,
+  currentStep,
+}) => (
+  <View>
+    <View style={styles.tooltipContainer}>
+      <Text testID="stepDescription" style={styles.tooltipText}>
+        {currentStep.text}
+      </Text>
+    </View>
+    <View style={styles.bottomBar}>
+      {!isLastStep ? (
+        <TouchableOpacity onPress={handleStop}>
+          <Button style={{ ...globalFontStyles.NB_13 }}>{"Skip"}</Button>
+        </TouchableOpacity>
+      ) : null}
+      {!isFirstStep ? (
+        <TouchableOpacity onPress={handlePrev}>
+          <Button style={{ ...globalFontStyles.NB_13 }}>{"Previous"}</Button>
+        </TouchableOpacity>
+      ) : null}
+      {!isLastStep ? (
+        <TouchableOpacity onPress={handleNext}>
+          <Button style={{ ...globalFontStyles.NB_13 }}>{"Next"}</Button>
+        </TouchableOpacity>
+      ) : (
+        <TouchableOpacity onPress={handleStop}>
+          <Button style={{ ...globalFontStyles.NB_13 }}>{"Finish"}</Button>
+        </TouchableOpacity>
+      )}
+    </View>
+  </View>
+);
+const StepNumberComponent = ({
+  isFirstStep,
+  isLastStep,
+  currentStep,
+  currentStepNumber,
+}) => (
+  <View style={styles.stepNumber}>
+    <Text style={[styles.stepNumberText]}>{currentStepNumber}</Text>
+  </View>
+);
+const MARGIN = 8;
+const WIDTH = Dimensions.get("window").width - 2 * MARGIN;
+export default copilot({
+  overlay: "svg", // or 'view'
+  animated: true, // or false
+  tooltipComponent: TooltipComponent,
+  tooltipStyle: {
+    width: WIDTH,
+    maxWidth: WIDTH,
+    left: MARGIN,
+  },
+  stepNumberComponent: StepNumberComponent,
+})(ContentPage);
 
 const styles = StyleSheet.create({
   header: {
-    height: 0.11 * height,
+    height: 0.113 * height,
     width: "100%",
     flexDirection: "row",
     shadowColor: "#000",
@@ -244,5 +375,48 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.18,
     shadowRadius: 1.0,
     elevation: 2,
+  },
+  walkableIcon: {
+    justifyContent: "flex-end",
+    alignItems: "center",
+    width: "50%",
+    height: "50%",
+    right: 0.05 * width,
+  },
+  areaPerIcon: { width: "20%", top: 20 },
+  tooltipContainer: { flex: 1 },
+  tooltipText: { ...globalFontStyles.NB_14, color: "#232323" },
+  bottomBar: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "flex-end",
+  },
+  stepNumber: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    borderWidth: 2,
+    borderRadius: 14,
+    borderColor: "#FFFFFF",
+    backgroundColor: "#FB5581",
+  },
+  stepNumberText: {
+    fontSize: 10,
+    backgroundColor: "transparent",
+    color: "#FFFFFF",
+  },
+  plannerTextDesign: {
+    ...globalFontStyles.NB_28,
+    color: "#FB5581",
+    bottom: 5,
+    left: 3,
+  },
+  barGraphHolderDesign: {
+    width: 50,
+    height: 50,
+    justifyContent: "center",
+    alignItems: "center",
+    overflow: "hidden",
+    left: 10,
   },
 });
